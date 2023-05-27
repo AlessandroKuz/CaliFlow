01b90d55f90e8f5fbb0488ec0b564ae77cbf1431
AlessandroKuz <alessandrokuz@gmail.com>
2023-05-27 22:57:42 +0200

    moved constants to separate file

--- scripts/main.py
+++ scripts/main.py
@@ -2,12 +2,7 @@ import datetime
 import time
 import os
 
-
-APP_NAME: str = "CaliFlow"
-DEFAULT_FILE_PATH: str = "exercises.txt"
-WORKOUT_TYPES: tuple[str, str] = ("Complex workout", "Simple workout")
-USER_TYPE: tuple[str, str] = ("New user", "Existing user")
-
+from settings import *
 
 
 """
---
+++ scripts/settings.py
@@ -0,0 +1,4 @@
+APP_NAME: str = "CaliFlow"
+DEFAULT_FILE_PATH: str = "exercises.txt"
+WORKOUT_TYPES: tuple[str, str] = ("Complex workout", "Simple workout")
+USER_TYPE: tuple[str, str] = ("New user", "Existing user")
