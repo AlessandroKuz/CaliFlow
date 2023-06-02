78661aaaaeb49e33c5e5fe673450b78fcf05c21d
AlessandroKuz <alessandrokuz@gmail.com>
2023-06-02 09:53:18 +0200

    Update settings with more explicative names

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -2,9 +2,9 @@ from utils.get_path import get_path
 
 
 APP_NAME: str = "CaliFlow"
-DATA_FILE_NAME: str = "personal_data.json"
+PERSONAL_DATA_FILE_NAME: str = "personal_data.json"
 DATA_FOLDER_NAME: str = "data"
-DEFAULT_PATH: str = get_path()
-DEFAULT_FILE_PATH: str = f"{DEFAULT_PATH}/{DATA_FOLDER_NAME}/{DATA_FILE_NAME}"
+CURRENT_MAIN_PATH: str = get_path()
+PERSONAL_DATA_FILE_PATH: str = f"{CURRENT_MAIN_PATH}/{DATA_FOLDER_NAME}/{PERSONAL_DATA_FILE_NAME}"
 WORKOUT_TYPES: tuple[str, str] = ("Complex workout", "Simple workout")
 USER_TYPE: tuple[str, str] = ("New user", "Existing user")
