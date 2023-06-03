0e2766197624293f4a6eba2ed6dd2b84d8570a5c
AlessandroKuz <alessandrokuz@gmail.com>
2023-06-03 21:51:53 +0200

    Implement raw idea of files setup

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -3,8 +3,11 @@ from utils.get_path import get_path
 
 APP_NAME: str = "CaliFlow"
 PERSONAL_DATA_FILE_NAME: str = "personal_data.json"
+WORKOUT_DATA_FILE_NAME: str = "workout_data.json"
 DATA_FOLDER_NAME: str = "data"
 CURRENT_MAIN_PATH: str = get_path()
-PERSONAL_DATA_FILE_PATH: str = f"{CURRENT_MAIN_PATH}/{DATA_FOLDER_NAME}/{PERSONAL_DATA_FILE_NAME}"
+DATA_FOLDER_PATH: str = f"{CURRENT_MAIN_PATH}/{DATA_FOLDER_NAME}"
+PERSONAL_DATA_FILE_PATH: str = f"{DATA_FOLDER_PATH}/{PERSONAL_DATA_FILE_NAME}"
+WORKOUT_DATA_FILE_PATH: str = f"{DATA_FOLDER_PATH}/{WORKOUT_DATA_FILE_NAME}"
 WORKOUT_TYPES: tuple[str, str] = ("Complex workout", "Simple workout")
 USER_TYPE: tuple[str, str] = ("New user", "Existing user")
